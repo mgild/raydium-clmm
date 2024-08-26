eb284cb591b5e390639a7952fdc0bd8f1e7e919b
mgild <mgild@users.noreply.github.com>
2024-08-26 03:03:19 -0400

    Update swap.rs

--- programs/amm/src/instructions/swap.rs
+++ programs/amm/src/instructions/swap.rs
@@ -123,19 +123,19 @@ pub struct SwapState {
 #[derive(Default)]
 pub struct StepComputations {
     // the price at the beginning of the step
-    sqrt_price_start_x64: u128,
+    pub sqrt_price_start_x64: u128,
     // the next tick to swap to from the current tick in the swap direction
-    tick_next: i32,
+    pub tick_next: i32,
     // whether tick_next is initialized or not
-    initialized: bool,
+    pub initialized: bool,
     // sqrt(price) for the next tick (1/0)
-    sqrt_price_next_x64: u128,
+    pub sqrt_price_next_x64: u128,
     // how much is being swapped in in this step
-    amount_in: u64,
+    pub amount_in: u64,
     // how much is being swapped out
-    amount_out: u64,
+    pub amount_out: u64,
     // how much fee is being paid in
-    fee_amount: u64,
+    pub fee_amount: u64,
 }
 
 pub fn swap_internal<'b, 'info>(
